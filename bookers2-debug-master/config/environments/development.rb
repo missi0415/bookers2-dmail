195206754f4b4e304766cbca53e3efe84c57de8e
missi0415 <76615633+missi0415@users.noreply.github.com>
2021-03-08 16:40:10 +0900

    Update development.rb

--- bookers2-debug-master/config/environments/development.rb
+++ bookers2-debug-master/config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   :port => 587,
   :domain => 'gmail.com',
   :user_name => 'missi0415@gmail.com',
-  :password => 'wccrqyfyygwkeeti',
+  :password => '**********',
   :authentication => :plain,
   :enable_starttls_auto => true
 }
